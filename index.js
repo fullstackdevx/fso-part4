263c8e57f10720ff608d8d90bef47bcd41576fc4
“fsdx” <fullstackdevx@gmail.com>
2021-03-05 11:21:43 +0100

    adds the controller

--- index.js
+++ index.js
@@ -4,31 +4,14 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
