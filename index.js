e89181bcfebcf5ff1b4a5d5e5db84337c83ce84b
“fsdx” <fullstackdevx@gmail.com>
2021-03-05 11:46:22 +0100

    moves the creation of the express app

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
