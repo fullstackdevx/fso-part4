df88136815d2b3fd2d80446f488ebf8a32091c45
“fsdx” <fullstackdevx@gmail.com>
2021-03-08 12:22:57 +0100

    helper functions and unit tests, step3

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -81,3 +81,95 @@ describe('total likes', () => {
     expect(listHelper.totalLikes(listWithBlogs)).toBe(36)
   })
 })
+
+describe('Favorite blog', () => {
+  const listWithOneBlog = [{
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  }]
+
+  const listOfBlogsWithOneFavorite = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listOfBlogsWithMoreThanOneFavorite = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  test('of no existing list returns undefined', () => {
+    expect(listHelper.favoriteBlog()).toBe(undefined)
+  })
+
+  test('of empty list is empty object', () => {
+    expect(listHelper.favoriteBlog([])).toEqual({})
+  })
+
+  test('when list has only one blog equals that', () => {
+    const expectedBlog = {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    }
+    expect(listHelper.favoriteBlog(listWithOneBlog)).toEqual(expectedBlog)
+  })
+
+  test('of a bigger list with only one favorite is calculated right', () => {
+    const expectedBlog = {
+      title: 'React patterns',
+      author: 'Michael Chan',
+      likes: 7
+    }
+    expect(listHelper.favoriteBlog(listOfBlogsWithOneFavorite)).toEqual(expectedBlog)
+  })
+
+  test('of a bigger list with many top favorite, returns the first one', () => {
+    const expectedBlog = {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    }
+    expect(listHelper.favoriteBlog(listOfBlogsWithMoreThanOneFavorite)).toEqual(expectedBlog)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (typeof blogs === 'undefined') return
+  if (blogs.length === 0) return {}
+
+  const favorite = blogs.reduce((favorite, blog) => favorite.likes < blog.likes ? blog : favorite)
+  return { title: favorite.title, author: favorite.author, likes: favorite.likes }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
