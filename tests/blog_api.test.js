0e43fc7e80279e0f1db45f9665b303e791e29cc1
“fsdx” <fullstackdevx@gmail.com>
2021-03-09 11:13:30 +0100

    bloglist tests, step1

---
+++ tests/blog_api.test.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (const blog of initialBlogs) {
+    const blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('returns the correct amount of blog posts', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
